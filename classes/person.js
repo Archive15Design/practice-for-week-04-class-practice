ae2aac4de753dac592eec672bc5c6e14311f2f10
Archive 15 Design <126675102+Archive15Design@users.noreply.github.com>
2023-05-12 10:32:40 +0200

    all tests passed

--- classes/person.js
+++ classes/person.js
@@ -1,5 +1,29 @@
 // Your code here
 
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  };
+
+  static introducePeople(personArray) {
+    if (Array.isArray(personArray) === false){
+      throw Error('introducePeople only takes an array as an argument.');
+    }
+    else if (personArray.every((person) => person instanceof Person) === false) {
+      throw Error('All items in array must be Person class instances.');
+    }
+    else {
+      personArray.forEach((person) => person.introduce());
+    }
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
